3ce89e8818950f3bf72b7e46591b714fbdeda63a
tomas-20 <tacuna20@stuy.edu>
2020-10-27 19:23:07 -0400

    created instance variables and constructor

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,10 @@
 public class BankAccount {
-  
+  double balance;
+  int accountID;
+  String password;
+  public BankAccount(int a, String p) {
+    balance = 0;
+    accountID = a;
+    password = p;
+  }
 }
