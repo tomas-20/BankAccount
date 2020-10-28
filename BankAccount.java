8ca9e2de1471a89a1ab60a58d1fd47f9cc0535cc
tomas-20 <tacuna20@stuy.edu>
2020-10-27 20:49:34 -0400

    made the deposit and withdraw

--- BankAccount.java
+++ BankAccount.java
@@ -10,4 +10,25 @@ public class BankAccount {
   public String toString() {
     return Integer.toString(accountID) + '\t' + Double.toString(balance);
   }
+  public boolean changeBalance(double amount, boolean type) {
+    boolean bad = false;
+    double n = amount;
+    if (type) {
+      bad = amount > balance;
+      n *= -1;
+    }
+    if (amount < 0 || bad) {
+      return false;
+    }
+    else {
+      balance += n;
+      return true;
+    }
+  }
+  public boolean deposit(double n) {
+    return changeBalance(n, false);
+  }
+  public boolean withdraw(double n) {
+    return changeBalance(n, true);
+  }
 }
--- Tester.java
+++ Tester.java
@@ -1,6 +1,11 @@
 public class Tester {
   public static void main(String[] args) {
     BankAccount bob = new BankAccount(2187, "super secret");
+    bob.deposit(-2.0);
+    bob.deposit(2000.0);
+    bob.withdraw(-2.0);
+    bob.withdraw(30.0);
+    bob.withdraw(1990.0);
     System.out.println(bob.toString());
   }
 }
